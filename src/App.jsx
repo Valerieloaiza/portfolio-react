fb169d44c105c45ec363e2d2071d2b4ac3f5c0c4
Valerie Calle <valerie.calle@gmail.com>
2025-03-20 12:54:58 -0500

    FIX

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Presenta } from "./Components/Pages/Home/Presenta";
 import { Header } from "./Components/Layouts/Header/Header";
 import { Routes, Route } from "react-router-dom";
 import { Skills } from "./Components/Pages/Skills/Skills";
-import { Container } from "./Components/Layouts/Container/container";
+import { Container } from "./Components/Layouts/Container/Container"
 import { Footer } from "./Components/Layouts/Footer/Footer";
 import { Studies } from "./Components/Pages/Studies/Studies";
 
